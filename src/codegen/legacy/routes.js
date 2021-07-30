5e8329925bbedcf65b4193e65ddec1c735ef5e5d
Adrian Acosta Mitjans <adrian.mitjans@gmail.com>
2021-07-30 08:14:12 -0700

    Update routes.js

--- src/codegen/legacy/routes.js
+++ src/codegen/legacy/routes.js
@@ -5,14 +5,14 @@ module.exports.routes = (obj) => {
 
     let content = "const express = require('express');\nconst router = express.Router();\n";
     content += "\n";
-    content += "const " + objname + " = require('../controllers/script.controller');\n";
+    content += "const " + objname + " = require('../controllers/" + obj.name.toLowerCase() + ".controller');\n";
     content += "\n";
 
-    content += "router.get('/', " + objname + "Ctrl.findAll);\n";
-    content += "router.get('/:id', " + objname + "Ctrl.findOne);\n";
-    content += "router.post('/', " + objname + "Ctrl.create);\n";
-    content += "router.put('/:id', " + objname + "Ctrl.update);\n";
-    content += "router.delete('/:id', " + objname + "Ctrl.delete);\n";
+    content += "router.get('/', " + objname + ".index);\n";
+    content += "router.get('/:id', " + objname + ".details);\n";
+    content += "router.post('/', " + objname + ".create);\n";
+    content += "router.put('/:id', " + objname + ".edit);\n";
+    content += "router.delete('/:id', " + objname + ".delete);\n";
     content += "\n";
     content += "export default router;";
 
